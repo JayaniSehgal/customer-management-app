d84441d5e3b07d3b2bb94a4c96bf73c3a00f45ea
Jayani Sehgal <jayanisehgal0803@gmail.com>
2025-04-02 12:36:58 +0530

    added the customer form and list

--- src/app/components/customers/customer-form/customer-form.component.ts
+++ src/app/components/customers/customer-form/customer-form.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-customer-form',
   templateUrl: './customer-form.component.html',
-  styleUrls: ['./customer-form.component.css']
+  styleUrls: ['./customer-form.component.css'],
 })
-export class CustomerFormComponent {
+export class CustomerFormComponent implements OnInit {
+  customerForm: FormGroup;
+  editingIndex: number | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private customerService: CustomerService,
+    private router: Router
+  ) {
+    this.customerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      address: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {
+    const storedCustomer = localStorage.getItem('editingCustomer');
+    if (storedCustomer) {
+      const customerData = JSON.parse(storedCustomer);
+      this.customerForm.patchValue(customerData.customer);
+      this.editingIndex = customerData.index;
+    }
+  }
+
+  onSubmmit(): void {
+    if (this.customerForm.valid) {
+      if (this.editingIndex !== null) {
+        this.customerService.editCustomer(
+          this.editingIndex,
+          this.customerForm.value
+        );
+      } else {
+        this.customerService.addCustomer(this.customerForm.value);
+      }
+      localStorage.removeItem('editingCustomer');
+      this.router.navigate(['/customers']);
+    }
+  }
 }
